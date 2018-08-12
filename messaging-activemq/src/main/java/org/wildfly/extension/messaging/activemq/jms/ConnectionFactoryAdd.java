87decdde58da98aff59b51b503c77adfb77774a4
Martyn Taylor <mtaylor@redhat.com>
2018-08-12 12:18:56 +0100

    Enable shared client ID on messaging CF

--- messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/jms/ConnectionFactoryAdd.java
+++ messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/jms/ConnectionFactoryAdd.java
@@ -157,6 +157,7 @@ public class ConnectionFactoryAdd extends AbstractAddStepHandler {
         config.setFactoryType(jmsFactoryType);
 
         config.setInitialMessagePacketSize(ConnectionFactoryAttributes.Common.INITIAL_MESSAGE_PACKET_SIZE.resolveModelAttribute(context, model).asInt());
+        config.setEnableSharedClientID(true);
         return config;
     }
 }
